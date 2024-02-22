aeb5e0054a3de394fe1e8a894c20f53d1dd98b2b
hankyung <hankyung@bjkang>
2024-02-22 14:19:54 +0900

    datasource를 tomcat에서 apache.common으로 변경

--- homepage_egov_testproject_v01/src/main/java/egovframework/example/config/EgovConfigDatasource.java
+++ homepage_egov_testproject_v01/src/main/java/egovframework/example/config/EgovConfigDatasource.java
@@ -1,6 +1,6 @@
 package egovframework.example.config;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
